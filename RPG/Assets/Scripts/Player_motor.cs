a1b40eddb28886cf975365fa21f5cb9a4c96ccab
Ivy7493 <60912637+Ivy7493@users.noreply.github.com>
2020-08-03 20:05:51 +0200

    First commit
    - added basic movement
    - added Camera tracking

---
+++ RPG/Assets/Scripts/Player_motor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_motor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float _speed;
+    void Start()
+    {
+        
+    }
+
+
+
+    void Motion()
+    {
+        float Xpos = Input.GetAxisRaw("Horizontal");
+        float Ypos = Input.GetAxisRaw("Vertical");
+        Vector3 Direction = new Vector3(Xpos, Ypos, 0f);
+        Direction *= _speed * Time.deltaTime;
+        transform.position += Direction;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Motion();
+    }
+}

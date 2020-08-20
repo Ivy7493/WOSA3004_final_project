00bbc6528d77d5e1f5aece536307d0a46f95cd2b
Ivy7493 <60912637+Ivy7493@users.noreply.github.com>
2020-08-20 15:19:56 +0200

    - System update
    - Added Fire Boss
    - Added Projectile Layers
    - Cleaned up Inspector

---
+++ RPG/Assets/Scripts/Enemy/AI/Fire_Boss_AI.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire_Boss_AI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] FireLocations;
+    public GameObject FireBombs;
+    GameObject Player;
+    float CurrentStage = 0;
+    float counter = 0f;
+    public float StageTime;
+    public float EngageRange;
+    public float CameraZoom;
+    public float SwipeRange;
+    public float SwipeDamageScale;
+    float CameraStart;
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        CameraStart = Camera.main.orthographicSize;
+        InvokeRepeating("CameraProspective", 0, 0.1f);
+    }
+
+
+    void RainFire()
+    {
+        float Select = Random.Range(0, FireLocations.Length);
+        for(int i = 0; i <= Select; i++)
+        {
+            Instantiate(FireBombs, FireLocations[i].transform.position, Quaternion.identity);
+        }
+
+    }
+
+    void Swipe()
+    {
+        if(Vector3.Distance(Player.transform.position, transform.position) < SwipeRange)
+        {
+            float Damage = GameObject.FindGameObjectWithTag("Experience_Manager").GetComponent<Experience_Manager>().ReturnLevel() * SwipeDamageScale;
+            GameObject.FindGameObjectWithTag("Resource_Manager").GetComponent<Resource_Manager>().Damage(Damage);
+        }
+    }
+
+    void CameraProspective()
+    {
+        if(Vector3.Distance(Player.transform.position, transform.position) < EngageRange)
+        {
+            float CameraSize = Camera.main.orthographicSize;
+            
+            if(CameraSize < CameraZoom)
+            {
+                Debug.Log("HERE!");
+                Camera.main.orthographicSize += 0.2f;
+            }
+            
+        }
+        else if(Vector3.Distance(Player.transform.position, transform.position) > EngageRange)
+        {
+            float CameraSize = Camera.main.orthographicSize;
+            if (CameraSize > CameraStart)
+            {
+                Camera.main.orthographicSize -= 0.2f;
+            }
+        }
+       
+    }
+
+    void SelectAbility()
+    {
+        if(Vector3.Distance(Player.transform.position,transform.position) < EngageRange)
+        {
+            counter += Time.deltaTime;
+            if (counter >= StageTime)
+            {
+                counter = 0;
+                switch (CurrentStage)
+                {
+                    case 0:
+                        RainFire();
+                        CurrentStage = 1f;
+                        break;
+                    case 1:
+                        Swipe();
+                        CurrentStage = 0f;
+                        break;
+                }
+            }
+        }
+       
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SelectAbility();
+    }
+}
---
+++ RPG/Assets/Scripts/Player/Player_Graphics.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Graphics : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Rotate()
+    {
+        if (Input.GetAxisRaw("Horizontal") == 1)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        else if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+
+        // Update is called once per frame
+        
+    }
+
+    private void Update()
+    {
+        Rotate();
+    }
+}

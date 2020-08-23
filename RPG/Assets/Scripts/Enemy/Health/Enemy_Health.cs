5571973ca56767d7e3538a480ee781dcb7ddf31f
Ivy7493 <60912637+Ivy7493@users.noreply.github.com>
2020-08-23 16:42:47 +0200

    - system improvement
    - added visual feedback for damage on enemies, you can enable and disable this feature on enemy prefabs by selecting the HitEffect bool

--- RPG/Assets/Scripts/Enemy/Health/Enemy_Health.cs
+++ RPG/Assets/Scripts/Enemy/Health/Enemy_Health.cs
@@ -10,14 +10,41 @@ public class Enemy_Health : MonoBehaviour
     float MaxHealth;
     float PlayerLevel;
     public GameObject HUD;
+
+
+    public bool HitEffect;
+    bool EffectPlaying = false;
+    SpriteRenderer Graphics;
+    float FlashCounter = 0f;
+    Color32 StartCol;
     void Start()
     {
         PlayerLevel = GameObject.FindGameObjectWithTag("Experience_Manager").GetComponent<Experience_Manager>().ReturnLevel();
+        Graphics = GetComponentInChildren<SpriteRenderer>();
         Health = PlayerLevel * HealthScale;
         MaxHealth = Health;
         if(HUD != null)
         {
             HUD.GetComponent<UI_Enemy_HUD>().UpdateHealthBar(1f);
+        }
+        StartCol = Graphics.color;
+    }
+
+
+
+    //Makes the enemy flash red on hit
+    void DamageEffect()
+    {
+        if(HitEffect == true && EffectPlaying == true)
+        {
+            FlashCounter += Time.deltaTime*10;
+            Graphics.color = Color.Lerp(StartCol, Color.red, FlashCounter);
+            if(FlashCounter >= 1)
+            {
+                FlashCounter = 0f;
+                EffectPlaying = false;
+                Graphics.color = StartCol;
+            }
         }
     }
 
@@ -35,6 +62,12 @@ public class Enemy_Health : MonoBehaviour
     public void Damage(float _damage)
     {
         Health -= _damage;
+        ///Will play the hit effect if not playing the hit effect
+        if(EffectPlaying != true)
+        {
+            EffectPlaying = true;
+        }
+        ///////////////////////////////////////////////////////
         if(HUD != null)
         {
             float temp = Health / MaxHealth;
@@ -49,6 +82,6 @@ public class Enemy_Health : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        DamageEffect();
     }
 }

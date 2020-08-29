f4a15381c9efa93311c9668214979d80b369bf26
Ivy7493 <60912637+Ivy7493@users.noreply.github.com>
2020-08-29 12:22:30 +0200

    - system update
    - chests!!!

---
+++ RPG/Assets/Scripts/Misc/Chest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    SpriteRenderer SR;
+    LootTable LT;
+    public Sprite ChestOpen;
+    float counter = 0.5f;
+    bool Opened = false;
+    void Start()
+    {
+        LT = GetComponent<LootTable>();
+        SR = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Opened = true;
+            SR.sprite = ChestOpen;
+        }
+    }
+
+    void ChestLogic()
+    {
+        if(Opened == true)
+        {
+            counter -= Time.deltaTime;
+            if(counter <= 0)
+            {
+                LT.DropLoot();
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChestLogic();
+    }
+}

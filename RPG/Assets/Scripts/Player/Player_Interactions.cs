384dc5509ae0521584c1ea3c081be719b8af5063
Gi-ddo <60913804+Gi-ddo@users.noreply.github.com>
2020-09-01 10:57:50 +0200

    Added Basic Tutorial

---
+++ RPG/Assets/Scripts/Player/Player_Interactions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Interactions : MonoBehaviour
+{
+    public UI_Tutorial tut;
+    [Header("Invoke timers")]
+    public float movementTut;
+    public float mage_encounter;
+    public float spell_encounter;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Point1"))
+        {
+            tut.activate_movement_txt();
+            tut.Invoke("deactivate_movement_txt", movementTut);
+        }
+
+        if (other.gameObject.CompareTag("Point2"))
+        {
+            tut.activate_mages_encounter();
+            tut.Invoke("deactivate_mages_encounter", mage_encounter);
+        }
+
+        if (other.gameObject.CompareTag("Point3"))
+        {
+            tut.activate_spell_items();
+            tut.Invoke("deactivate_spell_items", spell_encounter);
+        }
+    }
+}
---
+++ RPG/Assets/Scripts/UI/UI_Tutorial.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Tutorial : MonoBehaviour
+{
+    [Header("Gameplay Based Text")]
+    public GameObject Movement_txt;
+    public GameObject Spell_txt;
+
+    [Header("Story Based Text")]
+    public GameObject Start_txt;
+    public GameObject Mages_encounter;
+    //public GameObject Book_encounter;
+
+    
+
+
+
+    void Start()
+    {
+        Invoke("deactivate_start_txt", 8F);
+    }
+
+   
+    public void deactivate_start_txt()
+    {
+        Destroy(Start_txt);
+    }
+
+    public void activate_movement_txt()
+    {
+        Start_txt.SetActive(true);
+
+    }
+    public void deactivate_movement_txt()
+    {
+        Destroy(Movement_txt);
+    }
+
+
+
+    public void activate_mages_encounter()
+    {
+        Mages_encounter.SetActive(true);
+    }
+    public void deactivate_mages_encounter()
+    {
+        Destroy(Mages_encounter);
+    }
+
+    public void activate_spell_items()
+    {
+        Spell_txt.SetActive(true);
+    }
+    public void deactivate_spell_items()
+    {
+        Destroy(Spell_txt);
+    }
+
+   
+
+
+    
+
+}

8ca0681858c61524d501a882d12f1f75d8152420
Ivy7493 <60912637+Ivy7493@users.noreply.github.com>
2020-09-10 12:00:11 +0200

    - Alpha Bug Fixes
    - fixed enemies being cluncky
    - fixed collisions with game objects
    - fixed shitty book
    - fixed rotating health bars

--- RPG/Assets/Scripts/Misc/Book.cs
+++ RPG/Assets/Scripts/Misc/Book.cs
@@ -7,9 +7,25 @@ public class Book : MonoBehaviour
     // Start is called before the first frame update
     public UI_Tutorial UIT;
     bool open = false;
+    float counter = 3f;
     void Start()
     {
         
+    }
+
+
+    void TickDown()
+    {
+        if(open == true)
+        {
+            counter -= Time.deltaTime;
+            if (counter <= 0)
+            {
+                open = false;
+                UIT.deactivate_book_txt();
+            }
+        }
+       
     }
 
     
@@ -22,10 +38,6 @@ public class Book : MonoBehaviour
             {
                 UIT.activate_book_txt();
                 open = true;
-            }else if(open == true)
-            {
-                UIT.deactivate_book_txt();
-                open = false;
             }
            
 
@@ -35,6 +47,6 @@ public class Book : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        TickDown();
     }
 }

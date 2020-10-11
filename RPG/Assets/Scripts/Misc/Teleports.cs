bbfed68f43c3d243131fd7680ad10fa5e695e330
Gi-ddo <60913804+Gi-ddo@users.noreply.github.com>
2020-10-11 03:28:58 +0200

    Content patch
    -Added Teleports for ice and fire area after killing bosses back to central block

---
+++ RPG/Assets/Scripts/Misc/Teleports.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleports : MonoBehaviour
+{
+    public Transform destination;
+    public GameObject player;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Works");
+        player.transform.position = destination.transform.position;
+    }
+
+
+}

84484847144e38318c1a8d1177bab99e23bdb192
Ivy7493 <60912637+Ivy7493@users.noreply.github.com>
2020-10-11 13:05:31 +0200

    - bug patch
    - fixed teleport hiding character

--- RPG/Assets/Scripts/Misc/Teleports.cs
+++ RPG/Assets/Scripts/Misc/Teleports.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class Teleports : MonoBehaviour
 {
     public Transform destination;
-    public GameObject player;
+    GameObject player;
+
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        player.transform.position = destination.transform.position;
+        player.transform.position = new Vector3(destination.transform.position.x,destination.transform.position.y,0f);
     }
 
 

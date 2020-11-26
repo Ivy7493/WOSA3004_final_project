8e3adf0005934530b1738183aae7b0bfb6dbe0fe
Gi-ddo <60913804+Gi-ddo@users.noreply.github.com>
2020-11-26 02:51:48 +0200

    Content Patch
    - Credits scene links back to main menu after it ends

---
+++ RPG/Assets/Scripts/Credits.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+
+    float countdown = 26f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void transition()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= 1 * Time.deltaTime;
+
+        if (countdown <= 0)
+        {
+            transition();
+        }
+    }
+}
